953bc6ea976758cb83b68d5d218a90bd66e3a6ab
nmke-de <github@nmke.de>
2023-07-11 22:39:22 +0200

    Added path support

--- src/main.v
+++ src/main.v
@@ -1,9 +1,14 @@
 module main
 
 import net
+import os
 
 fn main() {
-	mut connection := net.dial_tcp('localhost:1900')!
-	println(connection.read_line())
+	mut connection := net.dial_tcp('${os.args[1]}:1900')!
+	path := os.args[2]
+	connection.write_string('/${path}\r\n')!
+	for line := connection.read_line(); line != ''; line = connection.read_line() {
+		println(line)
+	}
 	connection.close()!
 }
